8dde5590bcf5ea40cba8609dc95a476320b38ca2
putrafahrezi <putrafahrezi89@gmail.com>
2024-05-06 11:59:26 +0700

    pembuatan jurusan

---
+++ app/Http/Controllers/Jurusancontroller.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Jurusan;
+
+class JurusanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $nomor = 1;
+        $jur = Jurusan::all();
+        return view('jurusan.index',compact('nomor','jur'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('jurusan.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $jur = new Jurusan;
+        $jur->kode = $request->kode;
+        $jur->jurusan = $request->nama;
+        $jur->save();
+
+        return redirect('/jurusan/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $jur = Jurusan::find($id);
+        return view('jurusan.edit',compact('jur'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $jur = Jurusan::find($id);
+        $jur->kode = $request->kode;
+        $jur->jurusan = $request->nama;
+        $jur->save();
+
+        return redirect('/jurusan/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\JurusanController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('master');
-});
+// Route::get('/', function () {
+//     return view('master');
+// });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//data jurusan
+Route::get('/jurusan/', [JurusanController::class, 'index'])->middleware('auth');
+Route::get('/jurusan/form/', [JurusanController::class, 'create'])->middleware('auth');
+Route::post('/jurusan/store/', [JurusanController::class, 'store'])->middleware('auth');
+Route::get('/jurusan/edit/{id}', [JurusanController::class, 'edit'])->middleware('auth');
+Route::put('/jurusan/{id}', [JurusanController::class, 'update'])->middleware('auth');
